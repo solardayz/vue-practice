76864a6d8a5930e65a0141594bb41bbd05e6aeb0
a-home <solardayz@naver.com>
2022-02-16 21:03:44 +0900

    InputBox 추가 개발

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem">
-    <button v-on:click="addTodo">add</button>
+  <div class="inputBox">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <button class="addContainer addBtn" v-on:click="addTodo">add</button>
   </div>
 </template>
 
@@ -15,6 +15,9 @@ export default {
   methods: {
     addTodo: function () {
       localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function () {
       this.newTodoItem = '';
     }
   }
@@ -22,5 +25,29 @@ export default {
 </script>
 
 <style scoped>
-
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+  border-style: solid;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9em;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
