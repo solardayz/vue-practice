4665e25f856681cdf2288c0380927a1628a3a742
a-home <solardayz@naver.com>
2022-02-18 22:34:04 +0900

    TodoList 개발 중

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    List
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        {{ todoItem }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoList"
+  name: "TodoList",
+  data: function () {
+    return {
+      todoItems: []
+    }
+  },
+  created: function () {
+    if(localStorage.length > 0){
+      for (let i = 0 ; i < localStorage.length ; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  }
 }
 </script>
 
